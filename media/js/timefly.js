19846fb4e470f04457485e7c9b2e30cfae22ebaf
Daniel Linhart <Lingo@Daniels-MacBook-Pro.local>
2016-10-29 19:33:53 +0200

    priprava Team.html

--- media/js/timefly.js
+++ media/js/timefly.js
@@ -1,7 +1,15 @@
 $(window).scroll(function(){
-$(".fa-chevron-circle-right").stop().animate({"marginTop": ($(window).scrollTop()) + "px", "marginLeft":($(window).scrollLeft()) + "px"}, "slow" );
+    if(($(window).scrollTop()) < ($(".chevron").height()-80) ){
+        $(".fa-chevron-circle-right").stop().animate({"marginTop": ($(window).scrollTop()) + "px"}, "slow" );
+    }
 });
 		
 $(window).scroll(function(){
-$(".fa-chevron-circle-left").stop().animate({"marginTop": ($(window).scrollTop()) + "px", "marginLeft":($(window).scrollLeft()) + "px"}, "slow" );
+    if(($(window).scrollTop()) < ($(".chevron").height()-80) ){
+        $(".fa-chevron-circle-left").stop().animate({"marginTop": ($(window).scrollTop()) + "px"}, "slow" );
+    }
+});
+
+$(function(){
+  $(".under_nav").css({"marginTop": ($(".navbar-fixed-top").height())+ "px"})
 });
