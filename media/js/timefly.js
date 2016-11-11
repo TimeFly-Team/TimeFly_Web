ae7a72cb7f291c6b22d4f3bae6413a8099d8e2c9
Daniel Linhart <Lingo@Daniels-MacBook-Pro.local>
2016-11-11 20:04:02 +0100

    oprava posuvnych sipok

--- media/js/timefly.js
+++ media/js/timefly.js
@@ -1,18 +1,21 @@
 $(window).scroll(function(){
-    if(($(window).scrollTop()) < ($(".chevron").height()-80) ){
-        $(".fa-chevron-circle-right").stop().animate({"marginTop": ($(window).scrollTop()) + "px"}, "slow" );
+    if(($(window).scrollTop()) < ($(".chevron").height()-200) ){
+        $(".fa-chevron-circle-right").stop().animate({"marginTop": ((($(window).height()) - ($(".slider").height())-50)/2 + ($(window).scrollTop()))- 42 + "px"}, "slow" );
     }
 });
 		
 $(window).scroll(function(){
-    if(($(window).scrollTop()) < ($(".chevron").height()-80) ){
-        $(".fa-chevron-circle-left").stop().animate({"marginTop": ($(window).scrollTop()) + "px"}, "slow" );
+    if(($(window).scrollTop()) < ($(".chevron").height()-200) ){
+        $(".fa-chevron-circle-left").stop().animate({"marginTop": ((($(window).height()) - ($(".slider").height())-50)/2 + ($(window).scrollTop()))- 42 + "px"}, "slow" );
     }
 });
 
 $(function(){
-  $(".under_nav").css({"marginTop": ($(".navbar-fixed-top").height())+ "px"})
+  $(".under_nav").css({"marginTop": ($(".navbar-fixed-top").height())+ "px"});
+  $(".fa-chevron-circle-left").stop().animate({"marginTop": ((($(window).height()) - ($(".slider").height())-50)/2 + ($(window).scrollTop()))- 42 + "px"}, "slow" );
+  $(".fa-chevron-circle-right").stop().animate({"marginTop": ((($(window).height()) - ($(".slider").height())-50)/2 + ($(window).scrollTop()))- 42 + "px"}, "slow" );
 });
+
 function ContactToogle(open,close){
         $(open).css({"display": "block"});
         $(close).css({"display": "none"});
