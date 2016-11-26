fa76f5a52b4e061ea89a306cdea8b102ef79b336
px13 <peto13@gmail.com>
2016-11-26 20:41:43 +0100

    Opravená chyba, keď neprihlásenému užívateľovi, ktorého meno je už v databáze, vytvorilo nový záznam. Niektoré funkcie som trocha poupravil.

--- functions.php
+++ functions.php
@@ -30,7 +30,7 @@ function checkUserAndAddForum($conn, $name, $access = 0)
 
 function isLoggedUser()
 {
-	if ($_SESSION["loggedUser"] != NULL)
+	if (isset($_SESSION['loggedUser']) && !is_null($_SESSION["loggedUser"]))
 	{
 		return true;
 	}
@@ -39,11 +39,16 @@ function isLoggedUser()
 
 function addForum($conn, $name, $access = 0)
 {
-	if (mysqli_query($conn, "INSERT INTO forums (name, access) VALUES (\"".$name."\",".$access.")"))
+	if (mysqli_query($conn, getSqlInsertForum($name, $access)))
 	{
 		return mysqli_insert_id($conn);
 	}
 	return false;
+}
+
+function getSqlInsertForum($name, $access)
+{
+	return "INSERT INTO forums (name, access) VALUES (\"".$name."\",".$access.")";
 }
 
 //Pridanie témy
@@ -59,21 +64,21 @@ function checkUserAndAddTopic($conn, $forum_id, $name, $user_id = NULL)
 function existForumAndCanUserAddTopic($conn, $forum_id)
 {
 	$forum = getForumById($conn, $forum_id);
-	if ($forum == false)
+	if ($forum != false)
 	{
-		return false;
+		return canUserAddTopic($forum);
 	}
-	return canUserAddTopic($forum);
+	return false;
 }
 
 function getForumById($conn, $forum_id)
 {
 	$result = mysqli_query($conn, "SELECT * FROM forums f WHERE f.forum_id = ".$forum_id);
-	if (mysqli_num_rows($result) < 1)
+	if (mysqli_num_rows($result) > 0)
 	{
-		return false;
+		return mysqli_fetch_array($result);
 	}
-	return mysqli_fetch_array($result);
+	return false;
 }
 
 function canUserAddTopic($forum)
@@ -87,16 +92,20 @@ function canUserAddTopic($forum)
 
 function addTopic($conn, $forum_id, $name, $user_id = NULL)
 {
-	$sql_query = "INSERT INTO topics (forum_id, name) VALUES (".$forum_id.",\"".$name."\")";
-	if ($user_id != NULL)
-	{
-		$sql_query = "INSERT INTO topics (forum_id, name, user_id) VALUES (".$forum_id.",\"".$name."\",".$user_id.")";
-	}
-	if (mysqli_query($conn, $sql_query))
+	if (mysqli_query($conn, getSqlInsertTopic($forum_id, $name, $user_id)))
 	{
 		return mysqli_insert_id($conn);
 	}
 	return false;
+}
+
+function getSqlInsertTopic($forum_id, $name, $user_id)
+{
+	if (is_null($user_id))
+	{
+		return "INSERT INTO topics (forum_id, name) VALUES (".$forum_id.",\"".$name."\")";
+	}
+	return "INSERT INTO topics (forum_id, name, user_id) VALUES (".$forum_id.",\"".$name."\",".$user_id.")";
 }
 
 //Pridanie komentára
@@ -122,7 +131,7 @@ function existTopicAndCanUserAddComment($conn, $topic_id)
 function getTopicById($conn, $topic_id)
 {
 	$result = mysqli_query($conn, "SELECT * FROM topics t WHERE t.topic_id = ".$topic_id);
-	if (mysqli_num_rows($result) != 0)
+	if (mysqli_num_rows($result) > 0)
 	{
 		return mysqli_fetch_array($result);
 	}
@@ -131,7 +140,7 @@ function getTopicById($conn, $topic_id)
 
 function canUserAddComment($conn, $topic)
 {
-	if (isLoggedUser() || ($topic['user_id'] == NULL && getForumById($conn, $topic['forum_id'])['access'] < 2))
+	if (isLoggedUser() || (is_null($topic['user_id']) && getForumById($conn, $topic['forum_id'])['access'] < 2))
 	{
 		return true;
 	}
@@ -140,28 +149,10 @@ function canUserAddComment($conn, $topic)
 
 function addComment($conn, $topic_id, $text, $user_name = NULL)
 {
-	$user_id = getUserByNameOrCreateNewUser($conn, $user_name);
-	if ($user_id == false)
-	{
-		return false;
-	}	
-	if (mysqli_query($conn, "INSERT INTO comments (topic_id, user_id, text) VALUES (".$topic_id.",".$user_id.",\"".$text."\")"))
+	$user_id = isLoggedUser() ? getLoggedUserId() : getUserIdByNameOrCreateNewUser($conn, $user_name);
+	if ($user_id && mysqli_query($conn, getSqlInsertComment($topic_id, $user_id, $text)))
 	{
 		return mysqli_insert_id($conn);
-	}
-	return false;
-}
-
-function getUserByNameOrCreateNewUser($conn, $user_name)
-{
-	if (isLoggedUser())
-	{
-		return getLoggedUserId();
-	}
-	$user_id = createNewUser($conn, $user_name);
-	if ($user_id != false)
-	{
-		return $user_id;
 	}
 	return false;
 }
@@ -171,12 +162,28 @@ function getLoggedUserId()
 	return $_SESSION["loggedUser"];
 }
 
+function getUserIdByNameOrCreateNewUser($conn, $user_name)
+{
+	if (!is_null($user_name))
+	{
+		$user_id = getUserIdByName($conn, $user_name);
+		return  $user_id ? $user_id : createNewUser($conn, $user_name);
+	}
+	return false;
+}
+
+function getUserIdByName($conn, $user_name)
+{
+	$result = mysqli_query($conn, "SELECT u.user_id FROM users u WHERE u.name = \"".$user_name."\"");
+	if (mysqli_num_rows($result) > 0)
+	{
+		return mysqli_fetch_array($result)['user_id'];
+	}
+	return false;
+}
+
 function createNewUser($conn, $user_name)
 {
-	if ($user_name == NULL)
-	{
-		return false;
-	}
 	if (mysqli_query($conn, "INSERT INTO users (name) VALUES (\"".$user_name."\")"))
 	{
 		return mysqli_insert_id($conn);
@@ -184,5 +191,9 @@ function createNewUser($conn, $user_name)
 	return false;
 }
 
+function getSqlInsertComment($topic_id, $user_id, $text)
+{
+	return "INSERT INTO comments (topic_id, user_id, text) VALUES (".$topic_id.",".$user_id.",\"".$text."\")";
+}
 ?>
 
