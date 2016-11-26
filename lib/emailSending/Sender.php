92f688ae935a98164936fc6a15fde26408fa8f08
Peter Koza <koza9@uniba.sk>
2016-11-26 20:49:25 +0100

    auto e-mail sending

---
+++ lib/emailSending/Sender.php
@@ -0,0 +1,45 @@
+<?php
+
+require '../../vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+
+class Sender
+{
+        public function send($to, $message, $from)
+        {
+        	$mail = $this->setMail();
+        	$mail->setFrom('sktimefly@gmail.com', $from);
+			$mail->AddAddress($to);
+			$mail->Body = $message;
+			return $this->tryToSend($mail);
+        }
+
+        private function setMail()
+        {
+			$mail = new PHPMailer;
+			$mail->IsSMTP(); 
+			$mail->Host = 'smtp.gmail.com';
+			$mail->SMTPAuth = true;  
+			$mail->Username = "sktimefly@gmail.com";      
+			$mail->Password = "timefly12345";               
+			$mail->SMTPSecure = 'tls';  
+			$mail->Port = 587;
+			$mail->Subject = "TimeFly answer";
+			return $mail; 
+        }
+
+        private function tryToSend($mail)
+        {
+			if(!$mail->Send()) 
+			{
+			   echo 'Message was not sent.';
+			   echo 'Mailer error: ' . $mail->ErrorInfo;
+			   return false;
+			}
+			return true;
+		}
+}
+
+$sender = new Sender();
+$sender->send("koza9@uniba.sk", "skuska z timefly", "Karl");
+
+?>
