704984be1fa7c329ae0ff8f9476e21494b4a374a
Peter Koza <koza9@uniba.sk>
2016-11-27 11:13:55 +0100

    user

---
+++ lib/User.php
@@ -0,0 +1,49 @@
+<?php
+
+require 'functions.php';
+
+class User
+{
+    public function loggIn($name, $password)
+    {
+        if($this->verifyUser($name, $password)){
+            $_SESSION["name"] = $name;
+        }
+    }
+
+    public function logOut()
+    {
+        session_unset();    
+        session_destroy();
+    }
+
+    public function isLogged()
+    {
+    	return (isset($_SESSION['name']) && !is_null($_SESSION["name"]));
+    }
+
+    public function getName()
+    {
+        return $_SESSION['name'];
+    }
+
+    private function verifyUser($name, $password)
+    {
+        if ($con = db_connect()) { /* global function is used db_connect()*/
+            $sql = "SELECT * FROM users WHERE name='" .$name. "' and password='". $password ."' LIMIT 1";
+            $result = mysqli_query($con, $sql); 
+            if ($result && (mysqli_num_rows($result) > 0) ) {
+                mysqli_close($con);
+                return true;
+            } else {
+                 mysqli_close($con);
+                return false;
+            }
+        } else {
+            echo "Problem with connection2.";
+            return false;
+        }
+
+    }
+}
+?>
